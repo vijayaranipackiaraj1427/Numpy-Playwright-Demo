e0526e1615b2fa8da39a9778b8933623376ad386
Vijayarani Packiaraj <vijayaranipackiaraj1427@gmail.com>
2026-02-11 16:17:50 -0600

    updated datastructure testdata xlsx

--- steps/datastructureSteps.js
+++ steps/datastructureSteps.js
@@ -6,36 +6,52 @@ import { Home } from "../pom/Home.js";
 import { SignIn } from "../pom/SignIn.js";
 import { Timecomplexity } from "../pom/Timecomplexity.js";
 import { Tryeditor } from "../pom/Tryeditor.js";
+import { readExcel } from '../utils/excelReader.js';
+
+const testData = readExcel('testdata/ds-algo-testdata.xlsx', 'RegisterData');
 
 let datastructurePage;
 let signinPage;
 let homePage;
 let timecomplexityPage;
 let tryeditorPage;
+let username;
+let password;
 
-let username = 'vijayarani';
-let password = 'viji@123';
 
 Given('The user is on the DS Algo portal homepage', async ({ page }) => {
+
     await page.goto('https://dsportalapp.herokuapp.com/home');
+    console.log('test data ', testData);
+
+    // Read passwords from Excel (Row 1)
+
+    username = testData[1].username1;
+    password = testData[1].password2;
+
 });
 
 When('The user clicks the Sign In button and login successfully by entering user credentials', async ({ page }) => {
+
     homePage = new Home(page);
     await homePage.clickSignInLink();
     signinPage = new SignIn(page);
     await signinPage.fillUsername(username);
     await signinPage.fillPassword(password);
     await signinPage.clickSignInButton();
+
 });
 
 
 Then('The user should land on the Data Structure Home Page with message You are logged in', async ({ page }) => {
+
     await expect(page).toHaveURL('https://dsportalapp.herokuapp.com/home');
     await expect(page.getByText('You are logged in')).toBeVisible();
+
 });
 
 When('the user clicks the Getting Started button in Data Structures - Introduction', async ({ page }) => {
+
     homePage = new Home(page);
     await homePage.clickGetstarted();
 
@@ -48,56 +64,64 @@ Then('the user should land on the Data Structures - Introduction page', async ({
 });
 
 When('the user clicks the Time Complexity button', async ({ page }) => {
+
     homePage = new Home(page);
     await homePage.clickGetstarted();
 
     datastructurePage = new Datastructure(page);
     await datastructurePage.clickTimeComplexityButton();
+
 });
 
 Then('the user should be redirected to the Time Complexity page', async ({ page }) => {
 
     await expect(page.getByRole('paragraph').filter({ hasText: /^Time Complexity$/ })).toBeVisible();
+
 });
 
 When('the user clicks the Try Here button', async ({ page }) => {
+
     homePage = new Home(page);
     await homePage.clickGetstarted();
     datastructurePage = new Datastructure(page);
     await datastructurePage.clickTimeComplexityButton();
-
     timecomplexityPage = new Timecomplexity(page);
     timecomplexityPage.clickTryhereButton();
 });
 
 Then('the user should be redirected to a page having a Try Editor with a Run button to test', async ({ page }) => {
+
     await expect(page.locator('div').filter({ hasText: 'xxxxxxxxxx' }).nth(4)).toBeVisible();
     await expect(page.getByRole('button', { name: 'Run' })).toBeVisible();
 
 });
 
 When('the user clicks the Run button without entering code in the editor', async ({ page }) => {
+
     homePage = new Home(page);
     await homePage.clickGetstarted();
     datastructurePage = new Datastructure(page);
     await datastructurePage.clickTimeComplexityButton();
     timecomplexityPage = new Timecomplexity(page);
     timecomplexityPage.clickTryhereButton();
-
     tryeditorPage = new Tryeditor(page);
     await tryeditorPage.clickRunButton();
+
 });
 
 Then('the user should see no error message', async ({ page }) => {
+
     console.log("Nothing happend");
+
 });
 
 When('the user writes invalid Python code in the editor and clicks the Run button', async ({ page }) => {
+
     const invalidCode = `
-a = 5
-b = 10
-print(a + b)'
-`;
+    a = 5
+    b = 10
+    print(a + b)'
+    `;
     homePage = new Home(page);
     await homePage.clickGetstarted();
     datastructurePage = new Datastructure(page);
@@ -110,8 +134,8 @@ print(a + b)'
 
     // Invalid Python code (syntax error)
     await page.locator('.CodeMirror textarea').fill(invalidCode);
-
     await page.getByRole('button', { name: 'Run' }).click();
+
 });
 
 Then('the user should see an error message in the alert window', async ({ page }) => {
@@ -128,6 +152,7 @@ Then('the user should see an error message in the alert window', async ({ page }
 });
 
 When('the user writes valid Python code in the editor and clicks the Run button', async ({ page }) => {
+
     homePage = new Home(page);
     await homePage.clickGetstarted();
     datastructurePage = new Datastructure(page);
@@ -143,17 +168,18 @@ When('the user writes valid Python code in the editor and clicks the Run button'
         const editor = document.querySelector('.CodeMirror').CodeMirror;
         editor.setValue('');
     });
+    
     // Type valid Python code
     const validCode = `
-num1 = 10
-num2 = 15
+    num1 = 10
+    num2 = 15
 
-# Add two numbers
-sum = num1 + num2
+    # Add two numbers
+    sum = num1 + num2
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-    `;
+    # Display the sum
+    print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+        `;
 
     await page.locator('.CodeMirror textarea').fill(validCode);
     // Click Run AFTER typing
@@ -162,6 +188,7 @@ print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 });
 
 Then('the user should see output in the console', async ({ page }) => {
+
     const output = page.locator('#output');
 
     // Wait until Skulpt writes something into #output
@@ -176,4 +203,5 @@ Then('the user should see output in the console', async ({ page }) => {
 
     // Ensure output is not empty
     expect(text?.trim()).not.toBe('');
+
 });
